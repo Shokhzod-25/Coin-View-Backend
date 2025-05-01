4d310806ed8d89b2e1e79370f0c1b063d4e85913
Shahzod2555 <shahzodergashev2555@icloud.com>
2025-05-01 12:23:08 +0300

    Commit

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from src import create_app
 app = create_app()
 
 if __name__ == "__main__":
-	uvicorn.run("main:app", reload=True, host="127.0.0.1", port=9000)
+	uvicorn.run("main:app", reload=True, host="0.0.0.0", port=9000)
